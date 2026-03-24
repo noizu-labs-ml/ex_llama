b6492ea769296a9b35ac7ff0225bc09810c7a9c9
Keith Brings <keith.brings@noizu.com>
2026-03-24 19:39:57 +0700

    vsn bump

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule ExLLama.MixProject do
       name: "LLama CPP Nif Wrapper",
       description: description(),
       package: package(),
-      version: "0.2.0",
+      version: "0.2.1",
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
 
